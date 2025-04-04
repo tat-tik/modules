24773dab17747128986b715fdd6ed2c1345fdb67
Tanya <an5227952@gmail.com>
2025-04-04 18:17:52 +0300

    modules

---
+++ app.js
@@ -0,0 +1,5 @@
+
+import {default as Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+
+const game = new Game();
+game.start();
---
+++ game.js
@@ -0,0 +1,19 @@
+import Character from './domain';
+
+export {readGameSaving, writeGameSaving};
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+   function readGameSaving() {
+  }
+  
+   function writeGameSaving() {
+  }
+ 
