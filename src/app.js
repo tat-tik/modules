b282f1e6e45ec69dea155723555f458dce7d599f
Tanya <an5227952@gmail.com>
2025-04-11 17:42:11 +0300

    added webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-import {Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
 
 const game = new Game();
 game.start();
